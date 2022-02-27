06832d1267f2abdf484f2cfd54cae2e1ed5e3f3d
Ajay ps <ajaypalamkunnel45@gmail.com>
2022-02-27 10:16:06 +0530

    home screen completed

---
+++ lib/screens/home/screen_home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:money_manager/screens/category/screen_category.dart';
+import 'package:money_manager/screens/home/widgets/bottom_navigation.dart';
+import 'package:money_manager/screens/transactions/screen_transactions.dart';
+
+class ScreenHome extends StatelessWidget {
+  const ScreenHome({Key? key}) : super(key: key);
+
+  static ValueNotifier<int> selectedIndexNotifier = ValueNotifier(0);
+
+  final _pages = const [ScreenTransactions(), ScreenCategory()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: MoneyManagerBottomNavigation(),
+      body: SafeArea(
+          child: ValueListenableBuilder(
+              valueListenable: selectedIndexNotifier,
+              builder: (BuildContext context, int updatedIndex, _) {
+                return _pages[updatedIndex];
+              })),
+    );
+  }
+}
