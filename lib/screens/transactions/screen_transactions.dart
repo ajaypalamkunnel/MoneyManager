208b0952f36860f05a44bbcb887d4fd0d694f04b
Ajay ps <ajaypalamkunnel45@gmail.com>
2022-02-27 12:41:55 +0530

    home screen list view separated ui added

--- lib/screens/transactions/screen_transactions.dart
+++ lib/screens/transactions/screen_transactions.dart
@@ -5,6 +5,40 @@ class ScreenTransactions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("ScreenTransactions"));
+    return ListView.separated(
+      padding: EdgeInsets.all(10),
+      itemBuilder: (ctx, index) {
+        return Card(
+          color: Colors.indigo.shade500,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.indigo.shade900,
+              child: const Text(
+                "12 dec ",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 12),
+              ),
+            ),
+            title: const Text(
+              "10000",
+              style: TextStyle(color: Colors.white),
+            ),
+            subtitle: const Text(
+              'Travel',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (ctx, index) {
+        return SizedBox(
+          height: 10,
+        );
+      },
+      itemCount: 10,
+    );
   }
 }
